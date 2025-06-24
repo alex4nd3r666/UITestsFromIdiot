4d314e9fda62e519d3241fa0dfe6afe7b94a4413
CALLlA1011666 <lexa2268spb@gmail.com>
2025-06-24 23:27:50 +0300

    CALCULATOR

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,26 +1,92 @@
-#include "MainWindow.h"
-#include <AUI/Util/UIBuildingHelpers.h>
-#include <AUI/View/ALabel.h>
-#include <AUI/View/AButton.h>
+﻿#include "MainWindow.h"
 #include <AUI/Platform/APlatform.h>
-
+#include <AUI/Util/ALayoutInflater.h>
+#include <AUI/Util/UIBuildingHelpers.h>
+#include <AUI/View/AButton.h>
+#include <AUI/View/ADrawableView.h>
+#include <AUI/View/ALabel.h>
+#include <AUI/View/AScrollbar.h>
+#include <AUI/Util/ANoiseDrawable.h>
+#include <AUI/View/AText.h>
+#include <AUI/View/ATextField.h>
+#include <AUI/View/ASpinner.h>
+#include <AUI/View/ASpacerFixed.h>
+#include <AUI/ASS/Property/TextAlign.h>
+using namespace ass;
 using namespace declarative;
 
-MainWindow::MainWindow(): AWindow("Project template app", 300_dp, 200_dp) {
-    setContents(
-        Centered{
-            Vertical{
-                Centered { Label { "Hello world from AUI!" } },
-                _new<AButton>("Visit GitHub repo").connect(&AView::clicked, this, [] {
-                    APlatform::openUrl("https://github.com/aui-framework/aui");
-                }),
-                _new<AButton>("Visit docs").connect(&AView::clicked, this, [] {
-                    APlatform::openUrl("https://aui-framework.github.io/");
-                }),
-                _new<AButton>("Submit an issue").connect(&AView::clicked, this, [] {
-                    APlatform::openUrl("https://github.com/aui-framework/aui/issues/new");
-                }),
-            }
-        }
-    );
+MainWindow::MainWindow() : AWindow("Alexnλd3r's Delirium", 200_dp, 300_dp) {
+	AStylesheet::global().addRules({
+		{
+	t<AView>(),
+	BackgroundSolid { 0x000000_rgb },
+	TextColor{0xff0000_rgb},
+	//TextAlign::CENTER,
+  },
+  {
+	t<ALabel>(),
+
+	//TextAlign::CENTER,
+  },
+  {
+	t<ATextField>(),
+	//BackgroundSolid { AColor::RED },
+	//TextAlign::RIGHT,
+  },
+  {
+	t<AButton>(),
+	BackgroundSolid { 0x000000_rgb },
+	TextColor{0xff0000_rgb},
+	FixedSize{32_dp}
+	//TextAlign::CENTER,
+  },
+		});
+	setContents(
+		Centered{
+		Vertical{
+			_new<ATextField>(),
+			SpacerFixed(8_dp),
+			//AText("PRAISE THE CODE"),
+			Horizontal{
+			_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),
+		},
+
+		Horizontal{
+			_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),
+		},
+		Horizontal{
+			_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),
+		},
+		Horizontal{
+			_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),_new<AButton>("1"),
+		},
+
+		SpacerFixed(32_dp),
+		Centered{
+		_new<AButton>("PRAISE THE CODE")
+		  .connect(&AView::clicked, this, [] { APlatform::openUrl("https://www.youtube.com/watch?v=iJtVDEx2HSk"); })
+			with_style {
+			FixedSize{128_dp, 16_dp},
+			
+				},
+				},
+//_new<ALabel>("PRAISE THE CODE"),
+},
+
+
+
 }
+);
+	}
+		/*
+		PRAISE THE CODE
+		░░░█░░█░░█░░░
+		░░░▓░░▓░░▓░░░
+		█░░▒▒▒▓▒▒▒░░█
+		▓░░░░░▓░░░░░▓
+		▒▒▒▒▓▓▓▓▓▒▒▒▒
+		░░░░░░▓░░░░░░
+		░░█▓▒▒▓▒▒▓█░░
+		░░░░░▓░▓░░░░░
+		░░░░▓░░░▓░░░░
+		░░░░░▓▓▓░░░░░*/
